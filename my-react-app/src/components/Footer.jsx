220d5ab1ee0363cb9e112aa3139d18a07f25a177
D-Brian <nubilabrian4@gmail.com>
2026-02-07 14:05:09 -0500

    assets updated
    updated assets and fixed errors

---
+++ my-react-app/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+   <footer class="footer">
+  <div class="footer-content">
+    <p>&copy; 2026 Brian Nubila. All Rights Reserved.</p>
+
+    <div class="footer-links">
+      <a href="index.html">Home</a>
+      <a href="about.html">About</a>
+      <a href="projects.html">Projects</a>
+      <a href="services.html">Services</a>
+      <a href="contact.html">Contact</a>
+    </div>
+  </div>
+</footer>
+  );
+}
+
+export default Footer;
